80c065d5d4d1fa08886c3405757c28bdacdc7014
Brian Sandoval <jbriansandoval@gmail.com>
2018-02-04 22:36:57 -0500

    work

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,61 +1,59 @@
-var heros = ["Iron Man", "Captian America", "Hulk"];
+$(document).ready(function(){
 
-$("#button-view").on("click", function() {
-  // Grabbing and storing the data-animal property value from the button
-  var searchInput = $(this).attr("#search-input");
+  var heros = ["Captin America", "Iron Man", "Hulk", ];
+  function renderButtons(){
+    $("#buttons-view").empty();
 
-  // Constructing a queryURL using the animal name
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-    searchInput + "&api_key=Fimj98p4Jk5l6ezs5NOl5XJTxHYK737y&limit=10";
+    for (var i = 0; i < heros.length; i++){
+      var a = $("<button>");
+      a.addClass("hero");
+      a.attr("data-name", heros[i]);
+      a.text(heros[i]);
+      $("#buttons-view").append(a);
+    }   
+  }
+  $(document).on("click", ".hero", displayCharacterInfo);
 
-  // Performing an AJAX request with the queryURL
-  $.ajax({
+  $("#add-search").on("click", function(event) {
+    event.preventDefault();
+    var hero = $("#search-input").val().trim();
+    heros.push(hero);
+    renderButtons();
+  });
+  
+  $("button").on("click", function(){
+    $("#showGifs").empty();
+    var herogif = $(this).attr("data-name");
+    var queryURL= "https://api.giphy.com/v1/gifs/search?q=" + herogif + "&api_key=dc6zaTOxFJmzC&limit=10";
+    
+    $.ajax({
       url: queryURL,
       method: "GET"
-    })
-    // After data comes back from the request
-    .done(function(response) {
-      console.log(queryURL);
+     })
+     .done(function(response) {
+       var results = response.data;
+       
+       for (var i = 0; i < results.length; i++) {
+         if (results[i].rating !== "r" && results[i].rating !== "pq-13") {
+           var gifDiv = $("<div class='item'>");
 
-      console.log(response);
-      // storing the data from the AJAX request in the results variable
-      var results = response.data;
+           var rating = results[i].rating;
+           
+           var p = $("<p>").text("Gif Rating: " + rating);
+           
+           var heroImage = $("<img>");
 
-      // Looping through each result item
-      for (var i = 0; i < results.length; i++) {
+           heroImage.attr("src", results[i].images.fixed_height.url);
 
-        // Creating and storing a div tag
-        var searchDiv = $("<div>");
+           gifDiv.append(p);
+           gifDiv.append(heroImage);
 
-        // Creating a paragraph tag with the result item's rating
-        var p = $("<p>").text("Rating: " + results[i].rating);
+           $("#showGifs").prepend(gifDiv);
 
-        // Creating and storing an image tag
-        var searchImage = $("<img>");
-        // Setting the src attribute of the image to a property pulled off the result item
-        searchImage.attr("src", results[i].images.fixed_height.url);
+         }
+       }
+     });
+  });
 
-        // Appending the paragraph and image tag to the animalDiv
-        searchDiv.append(p);
-        searchDiv.append(searchImage);
-
-        // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-        $("#showgifs").prepend(searchDiv);
-      }
-    });
+  renderButtons();
 });
-
-      // This function handles events where one button is clicked
-      $("#addsearch").on("click", function(event) {
-        // Preventing the buttons default behavior when clicked (which is submitting a form)
-        event.preventDefault();
-        // This line grabs the input from the textbox
-        var searchInput = $("#seach-input").val().trim();
-
-        // Adding the movie from the textbox to our array
-        heros.push(searchInput);
-        console.log("heros[]")
-        // Calling renderButtons which handles the processing of our movie array
-        renderButtons();
-
-      });
